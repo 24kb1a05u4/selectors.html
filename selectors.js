1b650876a363aafcdbe5e2e1969e09eb03214f12
24kb1a05u4 <24kb1a05u4@nbkrist.org>
2026-08-16 13:34:57 +0530

    Add files via upload

---
+++ selectors.js
@@ -0,0 +1,6 @@
+const buttonByName = document.getElementsByName("myButton");
+console.log("Button with name 'myButton':", buttonByName);
+const firstParagraph = document.querySelector(".description");
+console.log("First element with class 'description':", firstParagraph);
+const allDescriptions = document.querySelectorAll(".description");
+console.log("All elements with class 'description':", allDescriptions);
